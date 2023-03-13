59d4001544e89aeb1f5963a721b4a62d0e90ac56
sergyth <73670477+sergyth@users.noreply.github.com>
2023-03-13 14:56:16 +0100

    routing corrigé

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,13 +1,18 @@
-
+//import { useRouteError } from "react-router-dom";
 import {Link} from 'react-router-dom'
 import './ErrorPage.css'
 
 export default function ErrorPage() {
+  //const error = useRouteError();
+
   return (
     <div className='errorPage'>
       <p className="errorPage_code">404</p>
       <p className="errorPage_message">Oups! La page que vous demandez n'existe pas.</p>
       <Link to='/' className='errorPage_link'>Retourner sur la page d'accueil</Link>
+      {/* <p>
+        <i>{error.statusText || error.message}</i>
+      </p> */}
     </div>
   )
 }
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,47 +1,77 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 import cards from "../../datas/kasa.json"
 import Carousel from "../../components/Carousel"
 import Label from "../../components/Label"
 import Rating from "../../components/Rating"
 import Dropdown from "../../components/Dropdown"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faStar} from "@fortawesome/free-solid-svg-icons"
+import { faStar } from "@fortawesome/free-solid-svg-icons"
 import "./Logement.css"
+import ErrorPage from "../ErrorPage"
 
 const Logement = () => {
   const { cardId } = useParams()
-  const card = cards.find((card) => card.id === cardId)
-  const {id, title, location, tags } = card
+  const [card, setCard] = useState(null)
 
-  let starList = []
-  for (let i = 0; i < 5; i++) {
-    starList.push( <FontAwesomeIcon
-      icon={faStar}
-      key={`rating-${i}`}
-      className={i > (card.rating - 1)? "white-star star": "star"}
-    />)
+  useEffect(() => {
+    loadCard()
+  })
+
+  const displayRating = () => {
+    let starList = []
+    for (let i = 0; i < 5; i++) {
+      starList.push(
+        <FontAwesomeIcon
+          icon={faStar}
+          key={`rating-${i}`}
+          className={i > card.rating - 1 ? "white-star star" : "star"}
+        />
+      )
+    }
+    return starList
   }
 
+  const loadCard = () => {
+    const card = cards.find((card) => card.id === cardId)
+    setCard(card)
+  }
+  if (card === undefined) return <ErrorPage />
+
   return (
-    <div className="logement-wrapper">
-      <div className="logement">
-        <Carousel id={id} title={title} card={card}  />
-      </div>
-      <div className="logement_presentation">
-        <Label title={title} location={location} tags={tags} />
-        <Rating picture={card.host.picture} name={card.host.name}>
-          {starList}
-        </Rating>
-      </div>
-      <ul className="logement_dropdown">
-        <Dropdown title="Description" className='logement_description'>{card.description}</Dropdown>
-        <Dropdown title="Equipements">
-          {card.equipments.map((equipement, index) => (
-            <span key={index} className='logement_equipement'>{equipement}</span>
-          ))}
-        </Dropdown>
-      </ul>
-    </div>
+    <>
+      {card && (
+        <div className="logement-wrapper">
+          <div className="logement">
+            <Carousel id={card?.id} title={card?.title} card={card} />
+          </div>
+          <div className="logement_presentation">
+            <Label
+              title={card?.title}
+              location={card?.location}
+              tags={card?.tags}
+            />
+            <Rating
+              picture={card?.host.picture}
+              name={card?.host.name}
+              children={displayRating()}
+            />
+          </div>
+          <ul className="logement_dropdown">
+            <Dropdown title="Description" className="logement_description">
+              {card?.description}
+            </Dropdown>
+            <Dropdown title="Equipements">
+              {card?.equipments.map((equipement, index) => (
+                <span key={index} className="logement_equipement">
+                  {equipement}
+                </span>
+              ))}
+            </Dropdown>
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
 export default Logement
